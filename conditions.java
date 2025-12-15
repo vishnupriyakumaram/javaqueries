522063ffe7d82546d56e2f62d5d4847d87441170
projectories <vishnupriyakumaram@gmail.com>
2025-12-15 18:43:27 +0530

    Initial commit

---
+++ conditions.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class conditions  {
+    public static void main(String args[])
+{
+    Scanner sc = new Scanner (System.in);
+//     int a = sc.nextInt();
+//     int b = sc.nextInt();
+//     if (a > b) {
+//         System.out.println("a is graeter");
+//     } else if(a == b) {
+//         System.out.println("b is greater");
+//     }
+// }    
+// }
+int button = sc.nextInt();
+switch(button){
+    case1(button == 1);
+        System.out.println("hello");
+        break;
+        case2(button == 2);
+            System.out.println("namaste");
+}
+}}
---
+++ index.java
@@ -0,0 +1,34 @@
+class Rectagle
+{
+    int length;
+    int breadth;
+    void getData(int x,int y)
+    {
+        length x;
+        breadth y;
+    }
+    int rectArea()
+    {
+        int area= length * breadth;
+        return area;
+    }
+}
+class index{
+    public static void main(String args[])
+    {
+        int area1,area2;
+        Rectangle r1=new Rectangle();
+        Rectangle r2=new Rectangle();
+        rect1.length=15;
+        rect1.breadth=20;
+        area=rect1.length * rect1.breadth;
+        rect2.getData(20,30);
+        area2=rect2.rectArea();
+        System.out.println("Area of rectangle 1: "+area1);
+        System.out.println("Area of rectangle 2: "+area2);
+
+    }
+}
+    
+
+
